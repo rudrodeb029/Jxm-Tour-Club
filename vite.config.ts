4aca024a3dcca9035f61134869b64f676bfe6973
rudrodeb029 <rudrodeb029@users.noreply.github.com>
2026-06-17 15:46:01 +0600

    Optimize build chunking and resolve chunk size warning

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) {
+              return 'vendor-firebase';
+            }
+            if (id.includes('react')) {
+              return 'vendor-react';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
+
